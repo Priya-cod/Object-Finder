95bc164623f05710311e77efb4066c23bf9be54c
Priya-cod <72642192+Priya-cod@users.noreply.github.com>
2021-06-06 14:59:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 img = "";
-
-function setup() {
-    canvas = createCanvas(640, 420);
-    canvas.center();
-}
+status = "";
 
 function preload(){
-ing = loadImage('dogcat.jpg');
+    img = loadImage('dogcat.jpg');
 }
 
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
 }
 
 function draw() {
     image(img, 0, 0, 640, 420);
     text("Dog", 45, 75);
     stroke("FF0000");
-    Rect(30, 60, 450, 350 );
+    rect(30, 60, 450, 350 );
+
+    text("Cat", 320, 120);
+    stroke("#FF0000");
+    rect(300, 90, 270, 320 );
 }
